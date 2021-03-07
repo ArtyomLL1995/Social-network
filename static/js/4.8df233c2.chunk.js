7cba1d059d186a1a4b0bde0363f123a5f4a04d94
Artyom Lysenko <artlysenka@mail.ru>
2021-03-07 18:49:07 +0300

    Updates

--- static/js/4.8df233c2.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-first-project"]=this["webpackJsonpreact-first-project"]||[]).push([[4],{378:function(t,c,e){"use strict";e.r(c);var r=e(0),s=(e(1),e(177));c.default=function(t){return Object(r.jsx)("div",{children:Object(r.jsx)(s.a,{})})}}}]);
-//# sourceMappingURL=4.8df233c2.chunk.js.map
